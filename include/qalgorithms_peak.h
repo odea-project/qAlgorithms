4e0e74d0c8b9a7fa6fa4341ca765c0de7dee2a0d
GeRe87 <89924726+GeRe87@users.noreply.github.com>
2024-01-31 16:24:19 +0100

    created the peak class

---
+++ include/qalgorithms_peak.h
@@ -0,0 +1,13 @@
+// qalgorithms_peak.h
+#ifndef QALGORITHMS_PEAK_H // Include guarde to prevent double inclusion
+#define QALGORITHMS_PEAK_H
+
+/* This file includes the q::peak class*/
+namespace q {
+  class Peak {
+    private:
+    public:
+  };
+}
+
+#endif // QALGORITHMS_PEAK_H
