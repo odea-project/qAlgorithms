92fdb121e1caeb23628f87e191c91869dfd98058
dahoehn <157411045+dahoehn@users.noreply.github.com>
2024-02-07 15:42:35 +0100

    Implement subsetBin function, functionality is limited to fragmenting a given index vector.

--- include/qalgorithms_qbin.h
+++ include/qalgorithms_qbin.h
@@ -4,26 +4,32 @@
 #include <math.h>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
+#include <functional>
 
 namespace q
 {
     // Functions
-    const std::vector<double> fastMeanDistances(const std::vector<double> &x);
+    const std::vector<double> fastMeanDistances(const std::vector<double> &x); // calculate the mean Distances of one to other elements in vector
+    // q::bin extractBin(const q::rawData &data);
+    // q::bin makeBins(const q::bin &bin);
+    //q::resultFrame ;
 
     // Classes
     class rawData
     {
     private:
-        std::vector<double> mz;
-        std::vector<double> rt;
+        std::vector<double> mz; // mass/charge ratio
+        std::vector<double> rt; // retention time
         std::vector<double> intensity;
+        std::vector<bool> retain; // T: keep point for further processing, F: Discard
 
     public:
         rawData();
         rawData(std::string path);
         ~rawData();
         double getmz(int i) const;
-        std::vector<double> getmz();
+        std::vector<double> getmz(); 
         void setmz(double value, int index);
         void setmz(const std::vector<double> &vec);
         double getrt(int i) const;
@@ -42,14 +48,29 @@ namespace q
     class bin
     {
     private:
-        std::vector<double> index;
+        std::vector<double> index; // link bin entries to dataset
         std::vector<double> os;
         double critVal; // ist es sinnvoll, für jeden bin ein neues Objekt zu erstellen?
         int groupsize;
+        int t_iterations; // test criteria
+        int binid; // checksum
+        std::string dataspace;   // type of data used
 
     public:
         bin();
         ~bin();
+        double getindex(int i);
+        std::vector<int> getindex();
+        void setindex(std::vector<int> vec);
+        double getos(int i);
+        std::vector<int> getos();
+        void setos(std::vector<int> vec);
+        int getcritVal();
+        void subsetBin(const std::vector<double> &nos,std::vector<int> idx);
+        /* fürs Subsetting ist es nicht notwendig, die nos zu verändern - 
+        aus den nos wird ein index-vektor erzeugt und rekursiv bearbeitet. 
+        Erst am Schluss müssen die Indices zurück auf die Daten gemappt werden */
+
     };
 
 }
