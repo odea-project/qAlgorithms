58732e36fe3e1d4e767643903dde66477c2edc0c
dahoehn <157411045+dahoehn@users.noreply.github.com>
2024-02-14 09:15:00 +0100

    expanded bincontainer, introduced unknown error

--- src/qalgorithms_qbin.cpp
+++ src/qalgorithms_qbin.cpp
@@ -35,32 +35,34 @@ namespace q
     RawData::RawData() {}
     RawData::RawData(std::string path) {}
     RawData::~RawData() {}
-    double RawData::getmz(int i) const
+    double RawData::getval_single(int col, int i) const
     {
-        return mz[i];
+        // return RawData[col][i];
+        return 1.3;
     }
-    std::vector<double> RawData::getmz(){
+    std::vector<double> RawData::getval(int col)
+    {
         return mz;
     }
-    void RawData::setmz(double value, int index) {}
-    void RawData::setmz(const std::vector<double> &vec)
-    {
-        mz = vec;
-    }
-    double RawData::getrt(int i) const { return 0.; }
-    // std::vector<double> RawData::getrt(){}
-    void RawData::setrt(double value, int index) {}
-    void RawData::setrt(const std::vector<double> &vec)
-    {
-        rt = vec;
-    }
-    double RawData::getintensity(int i) const { return 0.; }
-    // std::vector<double> RawData::getintensity(){}
-    void RawData::setintensity(double value, int index) {}
-    void RawData::setintensity(const std::vector<double> &vec)
-    {
-        intensity = vec;
-    }
+    void RawData::setval(int col, int i, double value) {}
+    // void RawData::setmz(const std::vector<double> &vec)
+    // {
+    //     mz = vec;
+    // }
+    // double RawData::getrt(int i) const { return 0.; }
+    // // std::vector<double> RawData::getrt(){}
+    // void RawData::setrt(double value, int index) {}
+    // void RawData::setrt(const std::vector<double> &vec)
+    // {
+    //     rt = vec;
+    // }
+    // double RawData::getintensity(int i) const { return 0.; }
+    // // std::vector<double> RawData::getintensity(){}
+    // void RawData::setintensity(double value, int index) {}
+    // void RawData::setintensity(const std::vector<double> &vec)
+    // {
+    //     intensity = vec;
+    // }
     void RawData::print()
     {
         std::cout << "success";
@@ -92,36 +94,76 @@ namespace q
     {
         std::cout << "\n Your documentation here! \n";
     }
-    const int RawData::size(){
+    const int RawData::size()
+    {
         return mz.size();
     }
 
     // BinContainer class
-    BinContainer::BinContainer(RawData rawData)
+
+    BinContainer::BinContainer(std::string user_desc) //, const RawData rawData, std::optional<std::vector<int>> orderInt, std::optional<std::vector<std::string>> &orderString
     {
-        const int n = rawData.size();
-        std::vector<int> index(n);
-        std::iota(index.begin(), index.end(), 1);
-        indices = index;
+        description = user_desc;
+        orderOfImportance = {0, 1}; // ßßß testing only, change later
+        // check if an input was made, default to first column as primary and second column as secondary parameter
+        // if (orderInt)
+        // {
+        //     // orderOfImportance = orderInt.get();
+        //     orderOfImportance = {0, 1};
+        // }
+        // if (orderString)
+        // {
+        //     orderOfImportance = {0, 1}; // ßßß
+        // }
+        // else
+        // {
+        //     orderOfImportance = {0, 1};
+        // }
+
+        // // assume rawData is a vector of vectors
+        // // sort by first OoI
+
+        // for (size_t i = 0; i < x.size(); ++i)
+        //     mainIndices.push_back(i);
+        // std::vector<int> mainIndices =
+        //     const int n = RawData.getval(orderOfImportance[0]);
+        // std::vector<int> index(n);
+        // std::iota(index.begin(), index.end(), 1);
+        // indices = index;
     }
-    void BinContainer::appendBin(Bin bin){
-        binStorage.push_back(bin);
+    void appendBin(Bin bin) // ßßß keep as part of BinStorage
+    {
+        //binStorage.push_back(bin);
         return;
     }
+    void BinContainer::makeNOS(int dimension, bool mostImportant)
+    {
+        if (mostImportant)
+        {
+            for (size_t i = 0; i+1 < mainIndices.size(); i++) // +1 to avoid diff outside the vector
+            {
+                double diff = mz[mainIndices[i]] - mz[mainIndices[i+1]];
+            }
+        }
+        else // produce new sorted index list for non-primary dimension
+        {
 
-    // Bin class
-    Bin::Bin() {}
-    Bin::~Bin() {}
+        }
+        activeNos.push_back(-1); // NOS vector has same length as data
+    }
 
+    int threadOpen = -1; // ßßß testing only, implemented as private variable in BinContainer
     void subsetBin(const std::vector<double> &nos, std::vector<int> idx) // idx als pointer
-    { // void ausgabe, extern vektor initialisieren und mit push_back() anfügen
+    {                                                                    // void ausgabe, extern vektor initialisieren und mit push_back() anfügen
         double vcrit;
         const int n = idx.size();
-        auto pmax = std::max_element(idx.begin(), idx.end()-2); // iterator with the position of maximum. -2 to not include maximum at which the previous cut occurred
+        auto pmax = std::max_element(idx.begin(), idx.end() - 2); // iterator with the position of maximum. -2 to not include maximum at which the previous cut occurred
         // int iterator must be implemented outside of the recursive function
         if (n < 5) // terminate function if Bin too small
         {
-            std::cout << "terminate at length" << n << "\n"; //ßßß testing only
+            std::cout << "terminate at length" << n << "\n"; // ßßß testing only
+            threadOpen = threadOpen + 1; // this thread terminates, reducing total threads by one
+            std::cout << "open threads: " << threadOpen << "\n";
             return;
         }
         if (n <= 100) // precalculate crit values if bottleneck
@@ -136,13 +178,16 @@ namespace q
         {
             // idx.push_back(-1); // not necessary with bin container
             for (int i = 0; i < n; ++i)
-                std::cout << idx[i] << " "; //ßßß testing only
-                std::cout << "\n";
-             // append Bin to bin container here
+                std::cout << idx[i] << " "; // ßßß testing only
+            std::cout << "\n";
+            threadOpen = threadOpen + 1; // this thread terminates, reducing total threads by one
+            // append Bin to bin container 
+            Bin output = Bin(idx);
+            //appendBin(output, target); // name of bin container? ßßß redundant if called through bin container, use that
         }
         else
         {
-
+            threadOpen = threadOpen - 1; // this thread terminates and creates two new ones, total increase = 1 thread
             std::vector<int> range1(idx.begin(), pmax);
             std::vector<int> range2(pmax, idx.end());
             subsetBin(nos, range1);
@@ -162,22 +207,30 @@ namespace q
 
     // return subsetBin(nos,);
 
+    // Bin class
+    Bin::Bin(std::vector<int> idx) {
+        index = idx;
+        binsize = index.size();
+    }
+    Bin::~Bin() {}
+
 }
 
 int main()
 {
     // test run: create raw data (mz only)
+    q::BinContainer("test_container"); // name?
 
 
-    const std::vector<double> nos = {0.0178,0.0179,0.0169,0.0175,0.0172,0.0173,0.5580,0.9373,0.2089,0.7187,0.8188,0.7409,0.5495,0.7000,0.7565,0.4286,0.4682,0.1984,0.3768,0.1503,0.2685,0.6151,0.8555,0.4497,0.4177,0.8574,0.2988,0.0278,0.6537,0.0783,0.6358,0.2581,0.7298,0.0919,0.2276,0.3038,0.7050,0.6696,0.7409,0.3830};
+    const std::vector<double> nos = {0.0178, 0.0179, 0.0169, 0.0175, 0.0172, 0.0173, 0.5580, 0.9373, 0.2089, 0.7187, 0.8188, 0.7409, 0.5495, 0.7000, 0.7565, 0.4286, 0.4682, 0.1984, 0.3768, 0.1503, 0.2685, 0.6151, 0.8555, 0.4497, 0.4177, 0.8574, 0.2988, 0.0278, 0.6537, 0.0783, 0.6358, 0.2581, 0.7298, 0.0919, 0.2276, 0.3038, 0.7050, 0.6696, 0.7409, 0.3830};
     std::vector<int> index(40); // function runs 12 times for the given dataset
     std::iota(index.begin(), index.end(), 1);
 
     q::subsetBin(nos, index);
 
-    std::vector<std::vector<int>> binContainer;
+    std::vector<std::vector<int>> binContainer{{-1}};
     binContainer.push_back(index);
-    // std::cout << binContainer[1][20]; // segmentation fault
+    std::cout << binContainer[1][1]; // segmentation fault !! Vektoren fangen mit Index 0 an !!
 
     // std::vector<double> x {1,2,3,4};
     // q::RawData test;
