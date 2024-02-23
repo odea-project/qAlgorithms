e062814e5dd0512762ae4406ddbcf22ad2200860
dahoehn <157411045+dahoehn@users.noreply.github.com>
2024-02-23 12:57:01 +0100

    integrated csv reader into qbin

--- test/readcsv.cpp
+++ test/readcsv.cpp
@@ -1,23 +1,17 @@
 #include <vector>
 #include <cmath>
-#include <chrono>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
 main()
 {
-    std::ifstream file("./test.csv");
+    std::ifstream file("./test.csv"); // test.csv must inclide a header line specifying the contents of each column
     std::vector<std::string> data_names;
     std::vector<std::vector<double>> data;
-    
-
     static bool initialised;
 
-    std::size_t line_number = 0;
     std::istringstream stream{};
-    
-    
 
     for (std::string line{}; std::getline(file, line); stream.clear())
     {
@@ -25,7 +19,7 @@ main()
         std::string number{};
         std::size_t position = 0;
 
-        if (!initialised)
+        if (!initialised) // only execute this part once - uses same naming as numbers to remove header
         {
             initialised = true;
             while (std::getline(stream, number, ','))
@@ -37,32 +31,17 @@ main()
             std::cout << "\n";
         }
 
-        while (std::getline(stream, number, ','))
-            {
-                std::cout << "number " << number << " -- ";
-                std::cout << "position " << position << " -- ";
-                data[position].push_back(stod(number));
-                ++position;
-                std::cout << "\n";
-            }
-        
-        //std::cout << "Line " << line_number++ << " contains " << nums << " col length " << columns << "\n";
+        while (std::getline(stream, number, ',')) // every subvector contains one column
+        {
+            std::cout << "number " << number << " -- ";
+            std::cout << "position " << position << " -- ";
+            data[position].push_back(stod(number));
+            ++position;
+            std::cout << "\n";
+        }
+
     }
     std::cout << data[2][2];
-    //     while (std::getline(file, line1)) {
-    //         std::cout << line1;
-    //       std::stringstream ss(line1);
-    //       std::string cell;
-    //       std::vector<double> row;
-
-    //       while (std::getline(ss, cell, ',')) {
-    //           row.push_back(std::stod(cell));
-    //       }
-
-    //       data.push_back(row);
-    //   }
-
-    //   std::cout << data[0][1];
 }
 
 //
