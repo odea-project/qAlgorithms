091fbceead12c5431480fd16a60146a4d96f4d3e
dahoehn <157411045+dahoehn@users.noreply.github.com>
2024-03-05 14:15:56 +0100

    added partially complete splitting function, compiles

--- include/qalgorithms_qbin.h
+++ include/qalgorithms_qbin.h
@@ -30,10 +30,10 @@ namespace q
     {
     private:
     public:
-        Bin(std::vector<int> idx);
+        Bin(std::vector<int> idx, double maxOS_in );
         ~Bin();
         std::vector<int> index; // link Bin entries to dataset
-        int binsize;                         // test criteria
+        double maxOS;   // remove need for re-sorting all bins after splitting
         double DQS_B;                        // Data quality score
     };
 
@@ -44,21 +44,27 @@ namespace q
         std::vector<int> mainIndices; // keep indices of sorted m/z for calculating DQS, implemented as deciding on one dimension
         /*calculate DQS for all dimensions and pick the best one?*/
         // std::vector<bool> retain; // T: keep point for further processing, F: Discard ßßß sollte redundant sein, da nur sortierte m/z bekannt sein müssen
-        std::vector<double> activeNos; // only one normalised order space is checked at a time ßßß calculate NOS per bin? Possibly less efficient, since bins never need to be reevaluated in one dimension
+        std::vector<double> activeNos; //ßßß rename; only one normalised order space is checked at a time ßßß calculate NOS per bin? Possibly less efficient, since bins never need to be reevaluated in one dimension
         std::vector<bool> dataspaceDone;    // prevent repeat operations, primarily intended for testing / time intensive calculations that are better performed stepwise
         std::vector<int> orderOfImportance; // first element is primary dimension
+        std::vector<int> scanNums;
         int errorCol;
-        struct OrderIndices                 // https://stackoverflow.com/questions/25921706/creating-a-vector-of-indices-of-a-sorted-vector
+        struct OrderIndices_double                 // https://stackoverflow.com/questions/25921706/creating-a-vector-of-indices-of-a-sorted-vector
         {
             const std::vector<double> &target;
-            OrderIndices(const std::vector<double> &target) : target(target) {}
+            OrderIndices_double(const std::vector<double> &target) : target(target) {}
+            bool operator()(int a, int b) const { return target[a] < target[b]; }
+        };
+        struct OrderIndices_int                 
+        {
+            const std::vector<int> &target;
+            OrderIndices_int(const std::vector<int> &target) : target(target) {}
             bool operator()(int a, int b) const { return target[a] < target[b]; }
         };
 
     public:
         BinContainer(const RawData &user_data); // , RawData rawData, std::optional<std::vector<int>> orderInt, std::optional<std::vector<std::string>>& orderString accept arguments as column number or name
         ~BinContainer();
-        // void selectDataspace(std::vector<int> selection); // adapt function as to accept different inputs (name), add option to ignore incomplete? ßßß superfluous
         void startProcessing();                                                  // wrapper function, iterate over all selected dataspaces
         void makeNOS(std::vector<double> activeDim); // set mainIndices if dataspace[0], set activeNos, only call once per dataspace
         void initBinning(int dataspace, RawData &user_data);                                         // generate Bins for any one dimension. dataspace is given as all dimensions to bin for as their column number in the imported csv. Binning is performed by iterating over dataspace
@@ -67,9 +73,9 @@ namespace q
         std::vector<int> byScore(double score, bool invert);      // only output bins over a given score, invert for below
         void t_binsizes();
         void subsetBin(const std::vector<double> &nos, const std::vector<double> &error, int beginBin, int endBin); // begin and end of range in nos
-        void splitBinByScans(const std::vector<double> &scanlist, std::vector<int> bin, const int maxdist);
+        void splitBinByScans(const std::vector<int> &scanNums, Bin &bin, const int maxdist, const std::vector<double> &error);
         const std::vector<double> meanDistances(const std::vector<double> &x); // calculate the mean Distances of one to other elements in vector
-        void fragmentBin(Bin, const std::vector<int> elutionOrder, int tolerance); // remove bins if separation in non-mass dimension is too great ßßß implement in subsetting routine?
+        void makeScanNums(const std::vector<double> &scanDim); // create vector containing the scan number for every entry in the dataset
         void help(); // include documentation for bin object
         // add identifier linking bin container to data which was read in
     };
