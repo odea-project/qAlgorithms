428c06abda333668d5a45d9d22c3f25adbc2f7c3
dahoehn <daniel.hoehn@stud.uni-due.de>
2024-03-14 01:10:43 +0100

    started on DQSB calculation

--- src/qalgorithms_qbin2.cpp
+++ src/qalgorithms_qbin2.cpp
@@ -4,17 +4,22 @@ namespace q
 {
     int subsetcount = 0;
 #pragma region "Featurelist"
-    FeatureList::FeatureList() {}
+    FeatureList::FeatureList()
+    {
+        scanBreaks = {0};
+    }
     FeatureList::~FeatureList() {}
     void FeatureList::readcsv(std::string user_file, int d_mz, int d_mzError, int d_RT, int d_scanNo)
     {
         std::ifstream file(user_file);
 
-        if (file.good())
-            std::cout << "failed to open file\n";
+        if (!file.good())
+            std::cout << "something is wrong with the input file\n";
         std::string line;
         std::string dummy;
         std::getline(file, dummy); // do not read first row ßßß data is probably not going to be passed with headers
+        int linecounter = 0;
+        int prevScan = 1;
         while (std::getline(file, line))
         {
             std::istringstream ss(line);
@@ -25,11 +30,20 @@ namespace q
             {
                 row.push_back(std::stod(cell));
             }
-#pragma GCC diagnostic push
+#pragma GCC diagnostic push // do not display the specific warning for rounding a double to integer
 #pragma GCC diagnostic ignored "-Wnarrowing"
-            Feature *F = new Feature{row[d_mz], row[d_mzError], row[d_RT], round(row[d_scanNo])}; // round gives warning, conversion functions as intended
+            int i_scanNo = round(row[d_scanNo]); // round gives warning, conversion functions as intended
 #pragma GCC diagnostic pop
+            Feature *F = new Feature{row[d_mz], row[d_mzError], row[d_RT], i_scanNo};
             allFeatures.push_back(F);
+            // check if scan changed from previous line
+            if (i_scanNo != prevScan)
+            {
+                prevScan = i_scanNo;
+                scanBreaks.push_back(linecounter); // scan == 1 from scanBreaks[0] to scanBreaks[1]-1 etc.
+            }
+
+            ++linecounter;
         }
     }
 #pragma endregion "Featurelist"
@@ -45,45 +59,47 @@ namespace q
         binDeque.push_back(firstBin);
     }
 
-    void BinContainer::subsetBins(std::vector<int> dimensions)
+    void BinContainer::subsetBins(std::vector<int> dimensions, int scanDiffLimit)
     {
-        // while
-        for (size_t i = 0; i < dimensions.size(); i++) // dimensions vector allows user to choose order of executing subsetting actions. It is also possible to only subset in mz or RT
+        // while elements are in the bin deque -> if any bin is not fully subset
+        while (!binDeque.empty())
         {
-            switch (dimensions[i])
+            for (size_t i = 0; i < dimensions.size(); i++) // dimensions vector allows user to choose order of executing subsetting actions. It is also possible to only subset in mz or RT
             {
-            case 1:
-            { // brackets needed to prevent error
-                // bin in mz
-                int startpoint = binDeque.size(); // startpoint is the first element of the deque that will not be subset, equivalent to do n times
-                for (size_t i = 0; i < startpoint; i++)
-                { // random access not needed, first element -> make bins -> move element or make bins, then remove first entry ; do n times
-                    binDeque.front().makeOS();
-                    binDeque.front().makeCumError();                                                                          // always after order space, since the features get sorted
-                    binDeque.front().subsetMZ(&binDeque, binDeque.front().activeOS, 0, binDeque.front().activeOS.size() - 1); // takes element from binDeque, starts subsetting, appends bins to binDeque
-                    binDeque.pop_front();                                                                                     // remove the element that was processed from binDeque
-                    // ßßß bad solution, needless copy if bin is ok - only relevant for controlling in rt, so should be fine here
-                }
-
-                break;
-            }
-
-            case 2:
-            {
-                // bin using scan numbers
-                int startpoint = binDeque.size();
-                for (size_t i = 0; i < startpoint; i++)
+                switch (dimensions[i])
                 {
-                    binDeque.front().subsetScan(&binDeque, 5);
-                    binDeque.pop_front();
+                case 1:
+                { // brackets needed to prevent error
+                    // bin in mz
+                    int startpoint = binDeque.size(); // startpoint is the first element of the deque that will not be subset, equivalent to do n times
+                    for (size_t i = 0; i < startpoint; i++)
+                    { // random access not needed, first element -> make bins -> move element or make bins, then remove first entry ; do n times
+                        binDeque.front().makeOS();
+                        binDeque.front().makeCumError();                                                                          // always after order space, since the features get sorted
+                        binDeque.front().subsetMZ(&binDeque, binDeque.front().activeOS, 0, binDeque.front().activeOS.size() - 1); // takes element from binDeque, starts subsetting, appends bins to binDeque
+                        binDeque.pop_front();                                                                                     // remove the element that was processed from binDeque
+                    }
+
+                    break;
                 }
 
-                break;
-            }
+                case 2:
+                {
+                    // bin using scan numbers
+                    int startpoint = binDeque.size();
+                    for (size_t i = 0; i < startpoint; i++)
+                    {
+                        binDeque.front().subsetScan(&binDeque, &finishedBins, scanDiffLimit);
+                        binDeque.pop_front();
+                    }
 
-            default:
-                std::cout << "\nSeparation method " << dimensions[i] << " is not a valid parameter, skipping... \n";
-                break;
+                    break;
+                }
+
+                default:
+                    std::cout << "\nSeparation method " << dimensions[i] << " is not a valid parameter, skipping... \n";
+                    break;
+                }
             }
         }
     }
@@ -158,7 +174,7 @@ namespace q
         }
     }
 
-    void Bin::subsetScan(std::deque<Bin> *bincontainer, const int &maxdist)
+    void Bin::subsetScan(std::deque<Bin> *bincontainer, std::vector<Bin> *finishedBins, const int &maxdist)
     {
         // sort
         int n = featurelist.size();
@@ -166,7 +182,8 @@ namespace q
                   { return lhs->scanNo < rhs->scanNo; });
         if (featurelist.front()->scanNo + n + maxdist > featurelist.back()->scanNo) // sum of all gaps is less than max distance, +n since no element will occur twice in one scan
         {
-            bincontainer->push_back(bincontainer->front()); // moves first element of que to last, since bin is good
+            // implement new list for elements that are binned to completion ßßß requires separation by scan numbers to always be done last - acceptable compromise?
+            finishedBins->push_back(bincontainer->front()); // moves first element of que to vector of complete bins, since bin is good
         }
         else
         {
@@ -205,8 +222,44 @@ namespace q
         // if prev - push back, pop front
     }
 
-    void Bin::makeDQSB(const std::vector<Feature *>)
+    void Bin::makeDQSB(const FeatureList *rawdata, const int &maxdist)
     {
+        // assumes bin is saved sorted by scans, result from scan gap checks being the final control
+        // only the minimum scans - k and the maximum scans + k need to be checked
+        // if a value m/z is not lower than the minimum of mz or larger than the maximum while being in the allowed scan interval, it is by definition included in the bin
+        int minScanNo = featurelist.front()->scanNo - maxdist - 1; // -1 to get index of scan interval vector
+        if (minScanNo < 0)
+            ; // first scan is scan number 0 in vector
+        {
+            minScanNo = 0;
+        }
+        int maxScanNo = featurelist.back()->scanNo + maxdist - 1;
+        if (maxScanNo > rawdata->scanBreaks.size() - 1)
+        {
+            maxScanNo = rawdata->scanBreaks.size() - 1;
+        }
+        // minScanNo to maxScanNo is the part of the feature list that needs to be checked
+        const auto [minMZ, maxMZ] = std::minmax_element(featurelist.front()->mz, featurelist.back()->mz);
+        int newBestMin = 225; // large number as first difference
+        int newBestMax = -225;
+        Feature *closestFmin;
+        Feature *closestFmax; // ßßß kontrolle, dass der Abstand nicht gleich groß ist
+
+        for (size_t i = rawdata->scanBreaks[minScanNo]; i < rawdata->scanBreaks[maxScanNo + 1] - 1; i++) // +1]-1 since the interval ends at the point one before the interval one greater starts
+        {
+            /* since both feature and raw data are ordered by scans, it should be possible to search by pointer address if this step takes too long */
+            if (std::find(featurelist.begin(), featurelist.end(), rawdata->allFeatures[i]) == featurelist.end()) // if the element is not found in the featurelist of the bin
+            {
+                if (0 < minMZ - rawdata->allFeatures[i]->mz < newBestMin)
+                {
+                    /* set as new best feature min */
+                }
+                if (0 < rawdata->allFeatures[i]->mz - newBestMax < newBestMax)
+                {
+                    /* set as new best feature min */
+                }
+            }
+        }
     }
 
 #pragma endregion "Bin"
@@ -218,12 +271,12 @@ int main()
     // std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
     // std::cout.rdbuf(result.rdbuf()); //redirect std::cout to out.txt!
     q::FeatureList testdata;
-    testdata.readcsv("G:/_Studium/Analytik-Praktikum/qbinning/rawdata/qCentroid_Warburg_pos_171123_01_Zu_01.csv", 0, 7, 5, 6); // ../../rawdata/qCentroid_Warburg_pos_171123_01_Zu_01.csv ../test/test.csv
+    testdata.readcsv("../../rawdata/qCentroid_Warburg_pos_171123_01_Zu_01.csv", 0, 7, 5, 6); // ../../rawdata/qCentroid_Warburg_pos_171123_01_Zu_01.csv ../test/test.csv
     q::BinContainer testcontainer;
     testcontainer.makeFirstBin(testdata);
     q::Bin testbin(testdata.allFeatures.begin(), testdata.allFeatures.begin() + 10);
     std::vector<int> dim = {1};
-    testcontainer.subsetBins(dim);
+    testcontainer.subsetBins(dim, 7);
     // std::cout.rdbuf(coutbuf); //reset to standard output again
     return 0;
 }
