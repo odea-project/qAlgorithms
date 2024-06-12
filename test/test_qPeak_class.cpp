322efb0c8837c8f9156926d8a7627da2aa0535ab
GeRe87 <89924726+GeRe87@users.noreply.github.com>
2024-06-12 10:31:31 +0200

    logo in separate file

--- test/test_qPeak_class.cpp
+++ test/test_qPeak_class.cpp
@@ -3,9 +3,7 @@
 // internal
 #include "../include/qalgorithms_measurement_data_lcms.h"
 #include "../include/qalgorithms_qpeaks.h"
-// #include "../include/qalgorithms_refmatrix.h"
-// #include "../include/qalgorithms_matrix.h"
-// #include "../include/qalgorithms_datatype_peak.h"
+#include "../include/qalgorithms_ascii_logo.h"
 
 // external
 #include "../external/StreamCraft/src/StreamCraft_mzml.hpp"
@@ -14,229 +12,15 @@
 #include <fstream>
 #include <cstdlib>
 #include <iomanip>
-#include <windows.h>
+
 int main()
 {
+  // print logo
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  q::logo::print_qpeaks();
   // initialize qPeaks static variables
   q::qPeaks::initialize();
   bool isCSV = false;
-
-  // print qPeaks LOGO in ascii art
-  // std::cout << "\033[1;31m"; // Bold red
-  // colors:
-  // bold red: \033[1;31m
-  // bold yellow: \033[1;33m 
-  // bold green: \033[1;32m
-  // bold cyan: \033[1;36m
-  // bold blue: \033[1;34m
-  // bold purple: \033[1;35m
-
-  HANDLE  hConsole;
-  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-  int k = 0;
-
-  // you can loop k higher to see more color choices
-  // for(k = 1; k < 255; k++)
-  // {
-  //   // pick the colorattribute k you want
-  //   SetConsoleTextAttribute(hConsole, k);
-  //   std::cout << k << " I want to be nice today!" << std::endl;
-  // }
-  SetConsoleTextAttribute(hConsole, 15);
-    std::cout << " _______  _______  _______  _______  ___   _  _______ \n";
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQQQQQQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "PPPPPPP";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 10);
-    std::cout << "EEEEEEE";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AAAAAAA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "KKK";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "| |";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "K";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << "SSSSSSS";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|\n";
-
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQQQQQQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "PPPPPPP";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 10);
-    std::cout << "EEEEEEE";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|_|";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "KKK";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|_|";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "K";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << "SSSSSSS";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|\n";
-
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "| |";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "PPP";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|_|";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "P";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 10);
-    std::cout << "EEE";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|___ |";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AAAAAAA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "KKKKKKK";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << "S";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|_____ \n";
-
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|_|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "PPPPPPP";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 10);
-    std::cout << "EEEEEEE";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AAAAAAA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "KKKKK";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|_ |";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << "SSSSSSS";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|\n";
-
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQQQQQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "| |";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "PPP";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|    |";
-    SetConsoleTextAttribute(hConsole, 10);
-    std::cout << "EEE";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|___ |";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AAAAAAA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "KKKKKKK";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "| _____|";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << "S";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|\n";
-
-    std::cout << "|";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "QQQQ";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 12);
-    std::cout << "Q";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 14);
-    std::cout << "PPP";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|    |";
-    SetConsoleTextAttribute(hConsole, 10);
-    std::cout << "EEEEEEE";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "| |";
-    SetConsoleTextAttribute(hConsole, 11);
-    std::cout << "AA";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "KKK";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "| |";
-    SetConsoleTextAttribute(hConsole, 13);
-    std::cout << "K";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "||";
-    SetConsoleTextAttribute(hConsole, 1);
-    std::cout << "SSSSSSS";
-    SetConsoleTextAttribute(hConsole, 15);
-    std::cout << "|\n\n\n\n";
 
   // userinput for filename
   std::string filename_input;
@@ -383,6 +167,6 @@ int main()
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   std::cout << "Press any key to exit" << std::endl;
   std::cin.get();
-  
+
   return 0;
 }
