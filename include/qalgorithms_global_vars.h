f4ebbc4dd2f858a5987f6be6aadb6329b1f52a98
GeRe87 <89924726+GeRe87@users.noreply.github.com>
2024-12-18 10:17:18 +0100

    declared initilize() in global vars

--- include/qalgorithms_global_vars.h
+++ include/qalgorithms_global_vars.h
@@ -1,17 +1,21 @@
 #ifndef _QALGORITHMS_GLOBALS_INCLUDED
 #define _QALGORITHMS_GLOBALS_INCLUDED
 
+#include <array>
+
 namespace qAlgorithms
 {
     extern float PPM_PRECENTROIDED;
     extern float MZ_ABSOLUTE_PRECENTROIDED;
     extern const double TOLERANCE_BINNING;
     extern const int GLOBAL_MAXSCALE;
-    extern std::array<float, 384> INV_ARRAY; // @todo make const
+    extern const std::array<float, 384> INV_ARRAY; // Make const
     extern const float T_VALUES[256];
     extern const float CHI_SQUARES[256];
     extern const double SQRTPI_2;
     extern bool PRINT_PPM_WARNING;
+
+    const std::array<float, 384> initialize(); // Declare initialize function
 }
 
 #endif
