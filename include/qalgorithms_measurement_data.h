ee3f0799a8abb3f28fdd772beab1eb37d8de42eb
dahoehn <157411045+dahoehn@users.noreply.github.com>
2025-01-23 12:17:16 +0100

    reworked assignment of relative scan number to be more rational. This changes results, but it is assumed some error was made in the assignments previously

--- include/qalgorithms_measurement_data.h
+++ include/qalgorithms_measurement_data.h
@@ -44,23 +44,16 @@ namespace qAlgorithms
                              float expectedDifference,
                              const bool updateExpectedDifference = true);
 
-    /**
-     * @brief A class to store 3D tensor measurement data, e.g., LC-MS data
-     * @details The TensorData class is a subclass of the MeasurementData class used to store 3D tensor measurement data, e.g., LC-MS data.
-     *
-     * @param data A 3D tensor of LC-MS data /ref DataType::LC_MS
-     */
-
     float calcRTDiff(std::vector<double> &retention_times);
 
     std::vector<dataPoint> mzmlToDataPoint(sc::MZML &data, const int index);
 
     std::vector<dataPoint> qbinToDataPoint(EIC &eic);
 
-    // methods
     /**
      * @brief Read 3D tensor data from a StreamCraft mzML object
-     * @details The readStreamCraftMZML method reads 3D tensor data from a StreamCraft mzML object and stores it in the TensorData object. A StreamCraft mzML object is created by using sc::MZML z("path to mzML file").
+     * @details The readStreamCraftMZML method reads 3D tensor data from a StreamCraft mzML object and stores
+     * it in the TensorData object. A StreamCraft mzML object is created by using sc::MZML z("path to mzML file").
      *
      * @param data : StreamCraft mzML object
      * @param ms1only : boolean to indicate if only MS1 spectra should be read
@@ -69,7 +62,6 @@ namespace qAlgorithms
      */
     std::vector<std::vector<CentroidPeak>> findCentroids_MZML(
         sc::MZML &data,
-        std::vector<unsigned int> &addEmpty,
         std::vector<float> &convertRT,
         float &rt_diff,
         const bool ms1only = true,
@@ -81,4 +73,4 @@ namespace qAlgorithms
         float rt_diff);
 }
 
-#endif // QALGORITHMS_MEASUREMENT_DATA_H
+#endif
