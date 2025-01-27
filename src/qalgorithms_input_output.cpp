5519519a06744cdaf25fdb261c0208b38240d639
dahoehn <157411045+dahoehn@users.noreply.github.com>
2025-01-27 13:24:44 +0100

    added cenID to output of bin printing

--- src/qalgorithms_input_output.cpp
+++ src/qalgorithms_input_output.cpp
@@ -583,9 +583,9 @@ namespace qAlgorithms
                       << "Filename: " << pathOutput << "\n";
             return;
         }
-        output << "ID,mz,mzUncertainty,scanNumber,retentionTime,area,areaUncertainty,"
+        output << "cenID,mz,mzUncertainty,scanNumber,retentionTime,area,areaUncertainty,"
                << "height,heightUncertainty,degreesOfFreedom,DQSC\n";
-        unsigned int counter = 1;
+        unsigned int counter = 0;
         for (size_t i = 0; i < peaktable.size(); i++)
         {
             if (!peaktable[i].empty())
@@ -689,14 +689,14 @@ namespace qAlgorithms
             return;
         }
         // @todo consider if the mz error is relevant when checking individual bins
-        output << "binID,mz,mzError,retentionTime,scanNumber,area,height,degreesOfFreedom,DQSC,DQSB\n";
+        output << "binID,cenID,mz,mzError,retentionTime,scanNumber,area,height,degreesOfFreedom,DQSC,DQSB\n";
         for (size_t binID = 0; binID < bins.size(); binID++)
         {
             for (size_t i = 0; i < bins[binID].mz.size(); i++)
             {
                 char buffer[128];
-                sprintf(buffer, "%zu,%0.8f,%0.8f,%0.4f,%d,%0.6f,%0.6f,%u,%0.4f,%0.4f\n",
-                        binID, bins[binID].mz[i], bins[binID].predInterval[i],
+                sprintf(buffer, "%zu,%u,%0.8f,%0.8f,%0.4f,%d,%0.6f,%0.6f,%u,%0.4f,%0.4f\n",
+                        binID, bins[binID].cenID[i], bins[binID].mz[i], bins[binID].predInterval[i],
                         bins[binID].rententionTimes[i], bins[binID].scanNumbers[i], bins[binID].ints_area[i],
                         bins[binID].ints_height[i], bins[binID].df[i], bins[binID].DQSC[i], bins[binID].DQSB[i]);
                 output << buffer;
