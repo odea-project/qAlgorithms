ee3c0b533bca730e433f6cd6962bd702ef922f02
dahoehn <daniel.hoehn@stud.uni-due.de>
2025-02-17 22:58:06 +0100

    todo: add way to perform regression or arbitrary 2d data read from file

---
+++ tools/getCoefficients.cpp
@@ -0,0 +1,13 @@
+#include "./qalgo_utility.h"
+#include <assert.h>
+#include "../include/qalgorithms_qpeaks.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    assert(argc == 2);
+
+    char coeffs[64]; // all coeffs with 15 digits
+    printf();
+    return 0;
+}
