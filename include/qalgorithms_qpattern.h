ed7a7da7521340383486c842026101140cbf7b67
dahoehn <157411045+dahoehn@users.noreply.github.com>
2025-03-07 14:48:10 +0100

    started transcribing the r script for tanimoto calculation to c++

--- include/qalgorithms_qpattern.h
+++ include/qalgorithms_qpattern.h
@@ -9,7 +9,14 @@
 
 namespace qAlgorithms
 {
+    // This function modifies the coefficients of a regression so that it describes a peak with the apex at
+    // x = RT of the feature and the height 1. Features are manipulated this way to allow a shape comparison
+    MovedRegression moveAndScaleReg(FeaturePeak *feature);
 
+    // calculate the shape tanimoto / jaccard index of two moved regressions. The score is weighted slightly.
+    // Calculation is done by dividing the area covered by the overlap both regression curves by the area covered
+    // by both regressions independently
+    float calcTanimoto(MovedRegression *feature_A, MovedRegression *feature_B);
 }
 
 #endif
