089cc7989b616e44e9d63ad137930cf67cc17591
dahoehn <daniel.hoehn@stud.uni-due.de>
2025-03-16 23:08:02 +0100

    removed one streamcraft compilation unit, added the score computing part of componentisation algorithm

--- include/qalgorithms_qpattern.h
+++ include/qalgorithms_qpattern.h
@@ -9,9 +9,21 @@
 
 namespace qAlgorithms
 {
+    void findComponents(const std::vector<FeaturePeak> *peaks);
+
+    struct ComponentGroup
+    {
+        std::vector<MovedRegression> features; // keep this sorted by intensity
+        std::vector<float> shapeScores;        // tanimoto scores on a per-feature basis. Access pattern [idx1] * number of features + [idx2]
+
+        // add a sample ID thing here eventually
+        float score(size_t idx1, size_t idx2);
+        void calcScores();
+    };
+
     // This function modifies the coefficients of a regression so that it describes a peak with the apex at
     // x = RT of the feature and the height 1. Features are manipulated this way to allow a shape comparison
-    MovedRegression moveAndScaleReg(FeaturePeak *feature);
+    MovedRegression moveAndScaleReg(const FeaturePeak *feature);
 
     // calculate the shape tanimoto / jaccard index of two moved regressions. The score is weighted slightly.
     // Calculation is done by dividing the area covered by the overlap both regression curves by the area covered
