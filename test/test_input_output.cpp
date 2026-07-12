9b9de5ed8a0b18951bc7890cea7af8eaf3acad99
dahoehn <daniel.hoehn@uni-due.de>
2026-07-12 23:30:52 +0200

    add missing main function to tests

--- test/test_input_output.cpp
+++ test/test_input_output.cpp
@@ -1,2 +1,7 @@
 #include "../src/qalgorithms_input_output.cpp" // NOLINT
 #include "common_test_utils.hpp"
+
+int main(void)
+{
+    return 0;
+}
--- test/test_qbin.cpp
+++ test/test_qbin.cpp
@@ -1,2 +1,7 @@
 #include "../src/qalgorithms_qbin.cpp" // NOLINT
 #include "common_test_utils.hpp"
+
+int main(void)
+{
+    return 0;
+}
